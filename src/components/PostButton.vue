0d65aae3ed4f6b343962dab42ed19a807cee80d1
K3yJey <kevin_madrid10@hotmail.es>
2023-10-13 19:59:01 -0500

    feat: :construction_worker: Se crearon componentes ejemplos y se implementaron

---
+++ src/components/PostButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="post-btn">Acceder</button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.post-btn {
+  padding: 15px;
+  background-color: blueviolet;
+  color: white;
+}
+
+.post-btn:hover {
+  background-color: white;
+  color: blueviolet;
+}
+</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-card">
+    <h3>Título</h3>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum atque eveniet ad
+      ut quam iste nostrum fugiat qui laboriosam autem praesentium dignissimos nam,
+      odio adipisci sed? Minima laudantium ex mollitia?
+    </p>
+
+    <PostButton />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PostButton from "./PostButton.vue"
+</script>
+
+<style scoped>
+.post-card {
+  border-radius: 15px;
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid #ccc;
+}
+</style>
