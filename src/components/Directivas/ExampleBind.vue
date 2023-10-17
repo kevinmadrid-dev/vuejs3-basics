1db85445b1d9ae6484ed026c649eb9fe78d4e9f8
K3yJey <kevin_madrid10@hotmail.es>
2023-10-17 12:15:58 -0500

    feat: :construction: Se agregó un evento de cambio de color mediante "v-on:click"

--- src/components/Directivas/ExampleBind.vue
+++ src/components/Directivas/ExampleBind.vue
@@ -5,10 +5,30 @@
   <p v-bind:style="{ backgroundColor: 'black', color: 'red' }">
     El mensaje se ha enviado correctamente
   </p>
+
+  <p v-bind:style="{ backgroundColor: bgcValue, color: colorValue }">Text</p>
+
+  <button @click="changeColor()">Cambiar color</button>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue"
+
 let hasError = false
+let bgcValue = ref("#fff")
+let colorValue = ref("#000")
+let darkMode = ref(false)
+
+const changeColor = () => {
+  darkMode.value = !darkMode.value
+  if (darkMode.value) {
+    bgcValue.value = "#000"
+    colorValue.value = "#fff"
+  } else {
+    bgcValue.value = "#fff"
+    colorValue.value = "#000"
+  }
+}
 </script>
 
 <style>
