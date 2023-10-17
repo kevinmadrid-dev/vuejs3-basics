dac47f3ee14089e3e63b62e9f46e2c447f187c73
K3yJey <kevin_madrid10@hotmail.es>
2023-10-17 09:52:29 -0500

    feat: :construction: Se añadió la directiva de "v-if", "v-for" y "v-bind"

---
+++ src/components/Directivas/ExampleBind.vue
@@ -0,0 +1,14 @@
+<template>
+  <p v-bind:class="{ danger: hasError }">El mensaje se ha enviado correctamente</p>
+  <p v-bind:class="{ danger: !hasError }">El mensaje no se ha enviado</p>
+</template>
+
+<script lang="ts" setup>
+let hasError = false
+</script>
+
+<style>
+.danger {
+  color: red;
+}
+</style>
