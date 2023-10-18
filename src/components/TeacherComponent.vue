31ef884fd1d3d8e37c7b2d61ad3ca2284819d575
K3yJey <kevin_madrid10@hotmail.es>
2023-10-18 11:45:04 -0500

    feat: :construction: Se reestructuró el componente de profesores

--- src/components/TeacherComponent.vue
+++ src/components/TeacherComponent.vue
@@ -1,69 +1,100 @@
 <template>
-  <h1 class="h1-title">Control de docentes</h1>
+  <section>
+    <h3>Añadir profesor</h3>
 
-  <form>
-    <div class="div-input-text">
-      <input type="text" placeholder="Nombres" v-model="name" /> <br />
-      <input type="text" placeholder="Apellidos" v-model="last_name" /> <br />
-      <input type="text" placeholder="DNI" v-model="dni" />
+    <div>
+      <label>Nombres<input type="text" v-model="teacher.teacherName" /></label>
+      <label>Apellidos<input type="text" v-model="teacher.teacherLastName" /></label>
+      <label>DNI<input type="text" v-model="teacher.teacherDni" /></label>
+      <label>Materias<input type="text" v-model="teacherSubject" /></label>
+      <button @click="handleSubject()">Añadir materias</button>
+      <ul>
+        <li v-for="(i_subject, index) in teacher.teacherSubjects" v-bind:key="index">
+          {{ i_subject }}
+        </li>
+      </ul>
+
+      <input type="checkbox" v-model="teacher.teacherDocs" /> Documentación entregada
+      <button @click="handleTeacher()">Añadir</button>
     </div>
+  </section>
 
-    <select class="select-option" v-model="inputSelect">
-      <option value="Matemáticas">Matemáticas</option>
-      <option value="Inglés">Inglés</option>
-      <option value="Programación I">Programación I</option>
-      <option value="Algoritmos">Algoritmos</option>
-      <option value="Introducción a la Ingenieria">
-        Introducción a la Ingenieria
-      </option>
-    </select>
+  <section>
+    <h3>Listado de profesores</h3>
 
-    <br />
-
-    <input type="checkbox" v-model="inputCheck" /> ¿Subió toda la documentación a la
-    web?
-  </form>
-
-  <div class="div-table">
-    <h2>Profesores registrados</h2>
-
-    <table class="table">
+    <table>
       <tr>
         <th>Nombres</th>
         <th>Apellidos</th>
         <th>DNI</th>
-        <th>Materia</th>
-        <th>Documentación</th>
+        <th>Materias</th>
+        <th>Documentación entregada</th>
       </tr>
 
-      <tr>
-        <th>{{ name }}</th>
-        <th>{{ last_name }}</th>
-        <th>{{ dni }}</th>
-        <th>{{ inputSelect }}</th>
-        <th>{{ inputCheck }}</th>
+      <tr v-for="i_teachers in teachers" v-bind:key="i_teachers.teacherDni">
+        <th>{{ i_teachers.teacherName }}</th>
+        <th>{{ i_teachers.teacherLastName }}</th>
+        <th>{{ i_teachers.teacherDni }}</th>
+        <th>
+          <ul>
+            <li
+              v-for="(i_materias, index) in i_teachers.teacherSubjects"
+              v-bind:key="index"
+            >
+              {{ i_materias }}
+            </li>
+          </ul>
+        </th>
+        <th v-if="i_teachers.teacherDocs">Entregada</th>
+        <th v-else>No entregada</th>
       </tr>
     </table>
-  </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
 import { Ref, ref } from "vue"
 
-let name: Ref = ref("")
-let last_name: Ref = ref("")
-let dni: Ref = ref("")
+interface ITeacher {
+  teacherName: string
+  teacherLastName: string
+  teacherDni: string
+  teacherSubjects: Array<string>
+  teacherDocs: boolean
+}
 
-let inputSelect: Ref = ref("")
+//Tipado de datos por Typescript
+let teacher: Ref<ITeacher> = ref({
+  teacherName: "",
+  teacherLastName: "",
+  teacherDni: "",
+  teacherSubjects: [],
+  teacherDocs: false
+})
 
-let inputCheck: Ref = ref("")
+let teachers: Ref<Array<ITeacher>> = ref([])
+let teacherSubject: Ref<string> = ref("")
 
-const checkConvert = () => {
-  if (inputCheck.value) {
-    inputCheck.value = "Sí"
-  } else {
-    inputCheck.value = "No"
-  }
+const handleSubject = () => {
+  teacher.value.teacherSubjects.push(teacherSubject.value)
+
+  teacherSubject.value = ""
+}
+
+const handleTeacher = () => {
+  teachers.value.push({
+    teacherName: teacher.value.teacherName,
+    teacherLastName: teacher.value.teacherLastName,
+    teacherDni: teacher.value.teacherDni,
+    teacherSubjects: teacher.value.teacherSubjects,
+    teacherDocs: teacher.value.teacherDocs
+  })
+
+  teacher.value.teacherName = ""
+  teacher.value.teacherLastName = ""
+  teacher.value.teacherDni = ""
+  teacher.value.teacherSubjects = []
+  teacher.value.teacherDocs = false
 }
 </script>
 
@@ -71,34 +102,5 @@ const checkConvert = () => {
 * {
   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
   text-align: center;
-}
-
-.h1-title {
-  text-transform: uppercase;
-}
-
-input,
-div {
-  margin: 5px 0px;
-}
-
-.div-table {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.table {
-  width: 80%;
-  text-align: center;
-  align-items: center;
-  border-collapse: collapse;
-}
-
-td,
-th {
-  border: 1px solid black;
-  text-align: center;
-  padding: 8px;
 }
 </style>
